450357d06be2ec43d44b9f98b5552364b8ad6eb5
hamed <hamed.sepehrnia85@gmail.com>
2025-11-17 14:43:30 +0330

    added whitenoise

--- Digito/settings.py
+++ Digito/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -156,6 +157,12 @@ STATIC_ROOT = Path(config('STATIC_ROOT', default=str(BASE_DIR / "staticfiles")))
 STATICFILES_DIRS = [
     str(BASE_DIR / "static"),
 ]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media files (User uploaded files)
 MEDIA_URL = "/media/"
